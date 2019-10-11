13081a0e7f8842f5e7c1398d6843600445b58a58
franout <angione96@libero.it>
2019-10-11 16:39:36 +0200

    aaaaaaaaaaaa

--- assignment_02_f/newton.c
+++ assignment_02_f/newton.c
@@ -287,14 +287,15 @@ static void * computation_task(void * args ) {
 					attr=888;
 					break;
 				}
+				if( cabs(x-1)<=1e-3){
 				for ( int k=0; k<LUT.n-2 ;k++ ){
 				      //TODO CHECK conditions	
-					if ( fabs(LUT.angles[k]-carg(x))<=1e-3  ) {
+					if ( fabs(LUT.angles[k]-carg(x))<=1  ) {
 						attr=carg(x);
 						break;
 					}
 
-				}
+				}}
 				if(attr!=0) {
 					break;
 				}	
