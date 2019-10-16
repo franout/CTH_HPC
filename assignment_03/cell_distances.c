84d43b4570aee3dcb568540e7324fee80146bc4f
franout <angione96@libero.it>
2019-10-16 14:51:35 +0200

    working reading of the files, check the insert to list and add a bufferized reading

--- assignment_03/cell_distances.c
+++ assignment_03/cell_distances.c
@@ -6,9 +6,9 @@
 #include <math.h>
 #include <string.h>
 #define INPUT_FILE "./coordinates.txt"
-#define ROW_L 24 // length of row  + \n
+#define ROW_L 24// length of row  + \n
 #define BUFFER_SIZE 2048 
-
+#define EPS 2
 /*ordered linked list definition */
 typedef struct node * node_p;
 
@@ -32,7 +32,7 @@ int main (int argc , char ** argv ) {
 	int option=0;
 	FILE * fp,*fp_1;
 	double dist,x1,x2,y1,y2,z1,z2;
-	char work_string[ROW_L-1],work_string_2[ROW_L-1];
+	char* work_string,* work_string_2; // +\n + \0
 	node_p head=NULL;
 	node_p x;
 	/*it returns the parsed char and as third arg it requires the separation char for arguments*/
@@ -50,31 +50,34 @@ int main (int argc , char ** argv ) {
 		}
 	}
 	omp_set_num_threads(N_THREAD);
-
+	work_string=(char *)calloc(ROW_L+1, sizeof(char));
+	work_string_2=(char *)calloc(ROW_L+1,sizeof(char));
+	if(work_string==NULL || work_string_2==NULL) {
+	fprintf(stderr,"error allocating strings\n");
+	exit(-1);
+	}
 	fp=fopen(INPUT_FILE,"r");
 	fp_1=fopen(INPUT_FILE,"r");
 	if(fp==NULL || fp_1==NULL) {
 		fprintf(stderr,"errror opening the file\n");
 		exit(-1);
 	}
-
 	int i=1;
-	fseek(fp_1,i*ROW_L+1,SEEK_CUR);
+	fseek(fp_1,i*(ROW_L),SEEK_SET);
 
 	while ((fread( work_string,sizeof(char), ROW_L,fp ))>0) {	
 		sscanf(work_string,"%lf %lf %lf\n",&x1,&y1,&z1);
 				int j=1;
-		/*while(fread(work_string_2,sizeof(char),ROW_L+1,fp_1)>0) {
-			//TODO we are eating chars somoewhere
-			sscanf(work_string_2,"%lf %lf %lf\n",&x1,&y2,&z2);
+	while(fread(work_string_2,sizeof(char),ROW_L,fp_1)>0) {
+			sscanf(work_string_2,"%lf %lf %lf\n",&x2,&y2,&z2);
 			dist=sqrt(pow(x2-x1 ,2)+pow(y2-y1,2)+pow(z2-z1,2));
-		//	add_to_list(head,dist);
-			printf("courple %s %s\n", work_string,work_string_2);
-		j++;
+			add_to_list(head,dist);
+	//	printf("dist - %.2lf\n",dist);
+				j++;
 		}
-*/
-				printf("%s\n",work_string);
-		fseek(fp_1,-ROW_L*j+i*ROW_L , SEEK_CUR);
+		
+		fseek(fp_1,-(ROW_L)*(j-2),SEEK_CUR);
+
 		i++;
 	}
 
@@ -98,10 +101,11 @@ int main (int argc , char ** argv ) {
 
 
 	free_list(head);
-
+	free(work_string);
+	free(work_string_2);
 	fclose(fp);
 	fclose(fp_1);
-	if(fp==NULL) {
+	if(fp==NULL || fp_1 ==NULL) {
 		fprintf(stderr,"error closing the file\n");
 		exit(-1);
 	}
@@ -174,6 +178,7 @@ static void add_to_list(node_p  head, double value){
 	}
 
 }
+
 
 
 static void free_list(node_p element) {
