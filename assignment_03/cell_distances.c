c4252b0a000e77b9bf3bef547e66aacb274a8753
franout <angione96@libero.it>
2019-10-17 12:53:15 +0200

    working on buffer io

--- assignment_03/cell_distances.c
+++ assignment_03/cell_distances.c
@@ -30,9 +30,10 @@ int main (int argc , char ** argv ) {
 
 	int N_THREAD=0;
 	int option=0;
-	int dist,x1,x2,y1,y2,z1,z2, tmp1,tmp2,tmp3;
+	double x1,x2,y1,y2,z1,z2, tmp1,tmp2,tmp3;
+	double dist;
 	char* work_string,* work_string_2,*tmp; // +\n + \0
-
+	long int read_char,read_char_2;
 	FILE * fp,*fp_1;
 	node_p head=NULL;
 	node_p x;
@@ -65,36 +66,39 @@ int main (int argc , char ** argv ) {
 		exit(-1);
 	}
 	int i=1;
-	x1=y1=z1=x2=y2=z2=tmp1=tmp2=tmp3=0;
+	x1=y1=z1=x2=y2=z2=tmp1=tmp2=tmp3=read_char=read_char_2=0;
 	fseek(fp_1,i*(ROW_L),SEEK_SET);
 
-	while ((fread( work_string,sizeof(char), ROW_L*BUFFER_SIZE,fp ))>0) {	
+	while ((read_char=fread( work_string,sizeof(char), ROW_L*BUFFER_SIZE,fp ))>0) {	
 		int j=1;
-		while(fread(work_string_2,sizeof(char),ROW_L,fp_1)>0) {
+		while((read_char_2=fread(work_string_2,sizeof(char),BUFFER_SIZE*ROW_L,fp_1))>0) {
 			// parallel part 
-			//
 			// if too slow switch second reading loop with the parsing buffer loop
-			for(int kb=0;kb<BUFFER_SIZE;kb++) {
+			for(int kb=0;kb<(BUFFER_SIZE);kb++) {
 				strncpy(tmp,work_string+kb*ROW_L,ROW_L);
-
-				sscanf(tmp,"%d.%d %d.%d %d.%d\n",&x1,&x2,&y1,&y2,&z1,&z2);		
-				x2+=x1*1000;
-				y2+=y1*1000;
-				z2+=z1*1000;
-
-				for(int kb_i=0;kb_i<BUFFER_SIZE;jb_i++) {
+//				printf("->%s",tmp);
+				/*sscanf(tmp,"%d.%d %d.%d %d.%d\n",&tmp1,&x2,&tmp2,&y2,&tmp3,&z2);		
+				x2+=tmp1*1000;
+				y2+=tmp2*1000;
+				z2+=tmp3*1000;
+*/
+				sscanf(tmp,"%f %f %f\n",&x2,&y2,&z2);
+				for(int kb_i=0;kb_i<(BUFFER_SIZE);kb_i++) {
 					strncpy(tmp,work_string_2+kb_i*ROW_L,ROW_L);
-					sscanf(tmp,"%d.%d %d.%d %d.%d\n",&tmp1,&x1,&tmp2,&y1,&tmp3,&z1);
+					//printf("--%s",tmp);
+					/*sscanf(tmp,"%d.%d %d.%d %d.%d\n",&tmp1,&x1,&tmp2,&y1,&tmp3,&z1);
 					x1+=tmp1*1000;
 					y1+=tmp2*1000;
 					z1+=tmp3*1000;
 					// compute distance
-					
-
+					dist=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)); */
+					sscanf(tmp,"%lf %lf %lf\n",&x1,&y1,&z1);
+					dist=sqrt(pow(x2-x1,2)+pow(y2-y1,2)+pow(z2-z1,2));
 					// add to a list CRITICAL section 	
+				printf("%.3lf\n",dist);
+					}
 
-				}
-
+		
 
 				j++;	
 			}
@@ -131,7 +135,7 @@ int main (int argc , char ** argv ) {
 }
 
 static void add_to_list(node_p  head, double value){
-	node_p x,new_node;
+	node_p x,y,new_node;
 	char str[6];
 	sprintf(str,"%.2f",value);
 
@@ -140,18 +144,31 @@ static void add_to_list(node_p  head, double value){
 	strcpy(new_node->value,str);
 	new_node->next=NULL;
 
-	if(head==NULL) 
-	{
-		head=new_node;
-
+	if(head==NULL) {
+		head=new_node;	
 	}
+
 	else {
-		for (x=head;x->next!=NULL;x=x->next) ;
-		x->next=new_node;
+		for(x=head;x!=NULL;x=x->next) {
+			if( strcmp(x->value,str)<=EPS || strcmp(x->value,str)>=-EPS ) {
+				// same value
+				x->occ++;
+				free(new_node);
+				break;
+			}  else if (strcmp(x->value,str)>0 && strcmp(x->next->value,str)<0 )  {
+				new_node->next=x->next;
+				x->next=new_node;
+				break;
+			}else if (strcmp(x->value,str)>0 && x->next==NULL)  {
+				
+				new_node->next=NULL;
+				x->next=new_node;
+				break;
+			}
+		}
 
 
-	}
-
+	}	
 	return ;
 }
 
