99a3d5dfb48784d054dc016e17e948511e4e7761
franout <angione96@libero.it>
2019-10-19 18:03:38 +0200

    working on error propagation

--- assignment_03/cell_distances.c
+++ assignment_03/cell_distances.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <math.h>
 #include <string.h>
-#define INPUT_FILE "./cell_e5"
+#define INPUT_FILE "./cells"
 #define ROW_L 24// length of row  + \n
 #define BUFFER_SIZE 2400
 
@@ -24,7 +24,8 @@ int main (int argc , char ** argv ) {
 
 	int N_THREAD=0;
 	int option=0;
-	int x1,x2,y1,y2,z1,z2,tmp1,tmp2,tmp3, dist;
+	float x1,x2,y1,y2,z1,z2;
+	 int	dist;
 	char* work_string,* work_string_2,*tmps,*tmps2; // +\n + \0
 	long int read_char,read_char_2;
 	FILE * fp,*fp_1;
@@ -44,12 +45,11 @@ int main (int argc , char ** argv ) {
 		}
 	}
 	omp_set_num_threads(N_THREAD);
-
 	work_string=(char *)calloc(BUFFER_SIZE+1, sizeof(char));
 	work_string_2=(char *)calloc(BUFFER_SIZE+1,sizeof(char));
 	tmps=(char *)calloc(ROW_L+1, sizeof(char));
 	tmps2=(char *) calloc(ROW_L+1,sizeof(char));
-	if(work_string==NULL || work_string_2==NULL || tmps==NULL|| tmps2==NULL) {
+	if(work_string==NULL || work_string_2==NULL || tmps==NULL) {
 		fprintf(stderr,"error allocating strings\n");
 		exit(-1);
 	}
@@ -59,7 +59,7 @@ int main (int argc , char ** argv ) {
 		fprintf(stderr,"errror opening the file\n");
 		exit(-1);
 	}
-	map.n=34640; // fp *100 -> max reacheable value of distance, with fixed input format
+	map.n=3465+1; // fp *100 -> max reacheable value of distance, with fixed input format
 	map.occ=(int *) malloc(sizeof(int)*map.n);
 	for(int i=0;i<map.n;i++){
 		map.occ[i]=0;
@@ -72,22 +72,22 @@ int main (int argc , char ** argv ) {
 		int j=1;
 		while((read_char_2=fread(work_string_2,sizeof(char),BUFFER_SIZE,fp_1))>0) {
 			// parallel part
-#pragma omp single 
+#pragma omp single
 
 			// if too slow switch second reading loop with the parsing buffer loop
 			for(int kb=0;kb<read_char-ROW_L;kb+=ROW_L) {
 				memcpy(tmps,work_string+kb,ROW_L);
-				sscanf(tmps,"%d.%d %d.%d %d.%d\n",&tmp1,&x2,&tmp2,&y2,&tmp3,&z2);
-				x2+=tmp1*1000;	y2+=tmp2*1000;	z2+=tmp3*1000;
-#pragma omp parallel for shared(kb,x2,y2,z2,read_char_2,map,work_string_2) private(x1,y1,z1,dist) 
-				for(int kb_i=kb;kb_i<read_char_2;kb_i+=ROW_L) {
+				sscanf(tmps,"%f %f %f\n",&x2,&y2,&z2);
+#pragma omp parallel for shared(kb,x2,y2,z2,read_char_2,map,work_string_2) private(x1,y1,z1,dist)
 
-					memcpy(tmps2,work_string_2+kb_i,ROW_L);;
-					sscanf(tmps,"%d.%d %d.%d %d.%d\n",&tmp1,&x1,&tmp2,&y1,&tmp3,&z1);
-					x1+=tmp1*1000;	y1+=tmp2*1000;	z1+=tmp3*1000;
-					dist=(int) (100*sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)));
+				for(int kb_i=kb;kb_i<read_char_2;kb_i+=ROW_L) {
+					memcpy(tmps2,work_string_2+kb_i,ROW_L);
+					sscanf(tmps2,"%f %f %f\n",&x1,&y1,&z1);
+					//TODO error in approximation of numbers
+					dist=round(sqrt(10* ((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)) ));
+/*					fprintf(stdout,"%lf %f  %f\n",sqrt( (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1) ),sqrt( (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1) ),sqrtf( (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1) ));*/
 #pragma omp atomic
-					map.occ[dist]++;
+					map.occ[dist]++;				
 				}
 			}
 			j++;
@@ -98,10 +98,14 @@ int main (int argc , char ** argv ) {
 
 	for(int i=0;i<map.n;i++) {
 		int occ=map.occ[i];
-		if(occ>=1) {
-			fprintf(stdout,"%.2f %d\n",i/100.00,occ);
+		if(occ>0){
+			if(i<1000) {
+				fprintf(stdout,"0%.2f %d\n",((float)(i))/100,occ);
 
-		}
+			}else {
+				fprintf(stdout,"%.2f %d\n",((float)(i))/100,occ);
+
+			}}
 	}
 	free(map.occ);
 	free(tmps);
