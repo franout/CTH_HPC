997092ec9e353765b29504d6b27c1e5c7f4eb44d
franout <angione96@libero.it>
2019-10-21 12:55:16 +0200

    missing kernels, buffers and kernel's enqueuing

--- assignment_04/heat_diffusion.c
+++ assignment_04/heat_diffusion.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include "./heat_diffusion.cl"
-#define INPUT_FILE "./diffusion_100_100"
+#include <math.h>
+#define CL_KERNEL_FILE "./heat_diffusion.cl"
+#define INPUT_FILE "./diffusion"
+#define MAX_KERNEL_SIZE 31457280 // 30 Mbytes
+
+
 
 /*open cl version 1.2*/
 #ifndef CL_TARGET_OPENCL_VERSION
@@ -17,12 +21,14 @@
 int main (int argc , char ** argv ) 
 {
 	int required_iterations=0;
-	double diffusion_const=0;
+	float diffusion_const=0;
 	int ret;
 	int option=0;
 	int h,w,x,y;
-	double ** matrix,val;
+	float  ** matrix,val,avg;
 	FILE *fp;
+	char * opencl_program_src;
+	long int size_cl_program=0;
 	/*it returns the parsed char and as third arg it requires the separation char for arguments*/
 	while ((option = getopt(argc,argv,"n:d:"))!=-1) {
 
@@ -43,21 +49,25 @@ int main (int argc , char ** argv )
 		}
 	}
 
+	avg=0;
 	fp=fopen(INPUT_FILE,"r");
 	if(fp==NULL) {
 		fprintf(stderr,"error openign the file \n");
 		exit(-1);	
 	}
 	/*saving input files*/
+	/*adding two additional rows and colums for avoididn to use control on the indexes*/
 	fscanf(fp,"%d %d\n",&h,&w);
-	matrix=(double **) malloc ( sizeof(double *) *h);
+	h+=2;
+	w+=2;
+	matrix=(float **) malloc ( sizeof(float  *) *h);
 	if(matrix==NULL) {
 		fprintf(stderr,"error allocating matrix\n");
 		exit(-1);
 	}
 
 	for(int i=0;i<h;i++) {
-		matrix[i]=(double *) malloc (sizeof(double) * w);
+		matrix[i]=(float *) malloc (sizeof(float) * (w));
 		if(matrix[i]==NULL)  {
 			fprintf(stderr,"errorr allocating row\n");
 			exit(-1);
@@ -65,43 +75,69 @@ int main (int argc , char ** argv )
 		}
 		/*obtaining a clean matrix and with boundaries at zero*/
 		for(int j=0;j<w;j++) {
-		matrix[i][j]=0;
-	
-		}
-
-
+			matrix[i][j]=0;
 
 		}
-		/*populating matrix reading from file*/
-		while(fscanf(fp,"%d %d %lf\n",&x,&y,&val)!=EOF) {
+
+
+
+	}
+	/*populating matrix reading from file*/
+	while(fscanf(fp,"%d %d %lf\n",&x,&y,&val)!=EOF) {
 		matrix[x][y]=val;
-		
-		}
+
+	}
 
 	fclose(fp);
 	if(fp==NULL) {
 		fprintf(stderr,"error closing the file\n");
 		exit(-1);
 	}
+	/*loading the kernel file*/
+	fp=fopen(CL_KERNEL_FILE,"r");
+	if(fp==NULL) {
+		fprintf(stderr,"error opening .cl file\n");
+		exit(-1);
+	}
 
+
+	opencl_program_src=(char *)calloc(MAX_KERNEL_SIZE,sizeof(char));
+	if(opencl_program_src==NULL) {
+		fprintf(stderr,"error allocating string for opencl program\n");
+		exit(-1);
+	}
+
+	size_cl_program=fread(opencl_program_src,sizeof(char),MAX_KERNEL_SIZE,fp);	
+
+
+	fclose(fp);
+	if(fp==NULL) {
+		fprintf(stderr,"error closing .cl file\n");
+		exit(-1);	
+	}
+	/*******************/
 	/*SET UP open cl*/
-	/*platform*/
-	cl_int error;
+	/****************/
 
+	/*data types of open cl declaration*/
+	cl_int error;
 	cl_platform_id platform_id;
 	cl_uint nmb_platforms;
 	cl_program program;
 	cl_context context;
 	cl_command_queue command_queue;
-		cl_kernel kernel;
-
+	cl_kernel kernel;
 	cl_device_id device_id;
 	cl_uint nmb_devices;
 
+	/*platform*/
 	if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
 		printf( "cannot get platform\n" );
 		return 1;
 	}
+
+
+
 	/*device */	
 
 	if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1,
@@ -111,8 +147,8 @@ int main (int argc , char ** argv )
 	}
 
 	/*context*/
-	
-	
+
+
 	cl_context_properties properties[] =
 	{
 		CL_CONTEXT_PLATFORM,
@@ -139,106 +175,110 @@ int main (int argc , char ** argv )
 	}
 
 
-//		/*memory reserved as buffers*/
-//		const size_t ix_m = 10e7;
-//		cl_mem input_buffer_a, input_buffer_b, output_buffer_c;
-//		input_buffer  = clCreateBuffer(context, CL_MEM_READ_ONLY,
-//				sizeof(float) * ix_m, NULL, &error);
-//		output_buffer_avg  = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-//				sizeof(float) * ix_m, NULL, &error);
-//		output_buffer_matrix = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-//				sizeof(float) * ix_m, NULL, &error);
-//
-//
-//		/*three last arguments are used for synchronization ( events ) */
-//		/*enqueuing writing buffers*/
-//		clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,
-//				0, ix_m*sizeof(float), a, 0, NULL, NULL);
-//		clEnqueueWriteBuffer(command_queue, input_buffer_b, CL_TRUE,
-//				0, ix_m*sizeof(float), b, 0, NULL, NULL);
-//
-//
-//
-//	/*open cl program command stored in a string */
-//
-////	program = clCreateProgramWithSource(context,1,
-//			(const char **) &opencl_program_src, NULL, &error);
-//
-//	/*error checking -> build log of the kernel*/
-//
-//	error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-//	if (error != CL_SUCCESS) {
-//		printf("cannot build program. log:\n");
-//
-//		size_t log_size = 0;
-//		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-//				0, NULL, &log_size);
-//
-//		char * log = calloc(log_size, sizeof(char));
-//		if (log == NULL) {
-//			printf("could not allocate memory\n");
-//			return 1;
-//		}
-//
-//		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-//				log_size, log, NULL);
-//		printf( "%s\n", log );
-//
-//		free(log);
-//
-//		kernel = clCreateKernel(program, "dot_prod_mul", &error);
-//
-//
-//
-//
-//		/*enqueuing kernel */
-//		clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_a);
-//		clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_b);
-//		clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer_c);
-//
-//		clEnqueueNDRangeKernel(command_queue, kernel, dimension,
-//				global_offsets, global_sizes, local_sizes, 0, NULL, NULL);
-//
-//		
-//
-//	for(size_t step=0;step<required_iterations ;step++) {
-//	
-//	
-//	
-//	}
-//
-//		/*print average temperature*/
-//		avg=avg/(h*w);
-//		fprintf(stdout,"Average temperature: %f\n",avg);
-//
-//		/*print absolute difference between entries of matrix and average tmp*/
-//		for(int i=0;i<h;i++) {
-//		for(int j=0;j<w;j++) {
-//		fprintf(stdout,"%f ",cabs(matrix[i][j]-avg));
-//		}
-//		fprintf(stdout,"\n");
-//		
-//		
-//		}
-//		clReleaseMemObject(input_buffer_a);
-//		clReleaseMemObject(input_buffer_b);
-//		clReleaseMemObject(output_buffer_c);
-//		//TODO use opencl data types
-//
-//
-//
-		/*release memory*/
-		clReleaseProgram(program);
-		clReleaseKernel(kernel);
-		clReleaseCommandQueue(command_queue);
-		clReleaseContext(context);
+	/*open cl program command stored in a string */
+	program = clCreateProgramWithSource(context,1,
+			(const char **) &opencl_program_src, NULL, &error);
 
-		for(int i=0;i<h;i++) {
-			free(matrix[i]);
+	/*error checking -> build log of the kernel*/
+
+	error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (error != CL_SUCCESS) {
+		printf("cannot build program. log:\n");
+
+		size_t log_size = 0;
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
+				0, NULL, &log_size);
+
+		char * log = calloc(log_size, sizeof(char));
+		if (log == NULL) {
+			printf("could not allocate memory\n");
+			return 1;
 		}
-		free(matrix);
 
-		return 0;
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
+				log_size, log, NULL);
+		printf( "%s\n", log );
+
+		free(log);
+		exit(-1);
 	}
+	kernel = clCreateKernel(program, "compute_diffusion", &error);
+	if(error!=CL_SUCCESS) {
+		fprintf(stderr,"cannot build kernel\n");
+		exit(-1);
+	}
+	//TODO
+	/*memory reserved as buffers*/
+	  const size_t ix_m = 10e7;
+	  cl_mem input_buffer_a, input_buffer_b, output_buffer_c;
+	  input_buffer  = clCreateBuffer(context, CL_MEM_READ_ONLY,
+	  sizeof(float) * ix_m, NULL, &error);
+	  output_buffer_avg  = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+	  sizeof(float) * ix_m, NULL, &error);
+	  output_buffer_matrix = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+	  sizeof(float) * ix_m, NULL, &error);
+
+
+	/*three last arguments are used for synchronization ( events ) */
+	/*enqueuing writing buffers*/
+		clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,
+		0, ix_m*sizeof(float), a, 0, NULL, NULL);
+		clEnqueueWriteBuffer(command_queue, input_buffer_b, CL_TRUE,
+		0, ix_m*sizeof(float), b, 0, NULL, NULL);
+
+/*
+ * ATTENTION: real index of matrix goes from 1 to n-1
+ */
+
+	
+	//TODO 	CHECK
+		for(size_t step=0;step<required_iterations ;step++) {
+		/*enqueuing kernel */
+		clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_a);
+		clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_b);
+		clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer_c);
+		clEnqueueNDRangeKernel(command_queue, kernel, dimension, 
+				global_offsets, global_sizes, local_sizes, 0, NULL, NULL);
+
+
+
+		}
+		
+	/*print average temperature*/
+
+	avg=avg/(h*w);
+	fprintf(stdout,"Average temperature: %f\n",avg);
+
+	/*print absolute difference between entries of matrix and average tmp*/
+
+	for(int i=0;i<h;i++) {
+		for(int j=0;j<w;j++) {
+			fprintf(stdout,"%f ",abs(matrix[i][j]-avg));
+		}
+		fprintf(stdout,"\n");
+
+
+	}
+	/*
+	   clReleaseMemObject(input_buffer_a);
+	   clReleaseMemObject(input_buffer_b);
+	   clReleaseMemObject(output_buffer_c);*/
+
+
+
+
+	/*release memory*/
+	clReleaseProgram(program);
+	clReleaseKernel(kernel);
+	clReleaseCommandQueue(command_queue);
+	clReleaseContext(context);
+
+	for(int i=0;i<h;i++) {
+		free(matrix[i]);
+	}
+	free(matrix);
+
+	return 0;
+}
 
 
--- assignment_04/heat_diffusion.cl
+++ assignment_04/heat_diffusion.cl
@@ -1,14 +1,22 @@
-__kernel void dot_prod_mul(
-	     __global const float * a,
-	     __global const float * b,
-	     __global float * c
-	     )
-{
+
+__kernel void compute_diffusion ( __global float **data, const int row, const int columns, const double  diff_c ,  ){
 
 
 /*kernel is afunction or a task to do Saved as a separate file (typically ending in .cl) and loaded on runtime */
+int ix=row+get_global_id(0);
+int jy=column+get_global_id(0);
 
 
-int ix = get_global_id(0);
-c[ix] = a[ix] * b[ix];
+float hl=data[ix][jy-1] ; //h(i,j-1)
+float hr=data[ix][jy+1] ;  // h(i,j+1)
+float ha= data[ix-1][jy]; //h(i-1,j)
+float hb=data[ix+1][jy] ; // h(i+1,j)
+float h=data[ix][jy];
+
+
+float local_t=h+diff_c(ha+hb+hl+hr/4*h);
+
+
+
+
 }
