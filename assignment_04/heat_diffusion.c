dce584951df2cfba022f069239fbf4cb6e1bd6da
franout <angione96@libero.it>
2019-10-21 11:04:15 +0200

    added setup of opencl enviroment

--- assignment_04/heat_diffusion.c
+++ assignment_04/heat_diffusion.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <CL/cl.h>
+#include <unistd.h>
+//#include "./heat_diffusion.cl"
 #define INPUT_FILE "./diffusion_100_100"
+
+/*open cl version 1.2*/
+#ifndef CL_TARGET_OPENCL_VERSION
+#define CL_TARGET_OPENCL_VERSION 120
+#endif
+
+
+#include <CL/cl.h>
 
 
 
@@ -13,6 +22,7 @@ int main (int argc , char ** argv )
 	int option=0;
 	int h,w,x,y;
 	double ** matrix,val;
+	FILE *fp;
 	/*it returns the parsed char and as third arg it requires the separation char for arguments*/
 	while ((option = getopt(argc,argv,"n:d:"))!=-1) {
 
@@ -33,7 +43,7 @@ int main (int argc , char ** argv )
 		}
 	}
 
-	fp=fopen(INPUT_FILE,'r');
+	fp=fopen(INPUT_FILE,"r");
 	if(fp==NULL) {
 		fprintf(stderr,"error openign the file \n");
 		exit(-1);	
@@ -45,6 +55,7 @@ int main (int argc , char ** argv )
 		fprintf(stderr,"error allocating matrix\n");
 		exit(-1);
 	}
+
 	for(int i=0;i<h;i++) {
 		matrix[i]=(double *) malloc (sizeof(double) * w);
 		if(matrix[i]==NULL)  {
@@ -52,9 +63,21 @@ int main (int argc , char ** argv )
 			exit(-1);
 
 		}
-		fscanf(fp,"%d %d %lf\n",&x,&y,&val);
+		/*obtaining a clean matrix and with boundaries at zero*/
+		for(int j=0;j<w;j++) {
+		matrix[i][j]=0;
+	
+		}
+
+
+
+		}
+		/*populating matrix reading from file*/
+		while(fscanf(fp,"%d %d %lf\n",&x,&y,&val)!=EOF) {
 		matrix[x][y]=val;
-	}	
+		
+		}
+
 	fclose(fp);
 	if(fp==NULL) {
 		fprintf(stderr,"error closing the file\n");
@@ -63,35 +86,45 @@ int main (int argc , char ** argv )
 
 	/*SET UP open cl*/
 	/*platform*/
+	cl_int error;
+
+	cl_platform_id platform_id;
+	cl_uint nmb_platforms;
+	cl_program program;
+	cl_context context;
+	cl_command_queue command_queue;
+		cl_kernel kernel;
+
 	cl_device_id device_id;
 	cl_uint nmb_devices;
+
+	if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
+		printf( "cannot get platform\n" );
+		return 1;
+	}
+	/*device */	
+
 	if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1,
 				&device_id, &nmb_devices) != CL_SUCCESS) {
 		printf( "cannot get device\n" );
 		return 1;
 	}
-	/*device */	
-	cl_int error;
-
-	cl_platform_id platform_id;
-	cl_uint nmb_platforms;
-	if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
-		printf( "cannot get platform\n" );
-		return 1;
-	}
-
 
 	/*context*/
-
-	cl_context context;
+	
+	
 	cl_context_properties properties[] =
 	{
 		CL_CONTEXT_PLATFORM,
 		(cl_context_properties) platform_id,
 		0
 	};
-	context = clCreateContext(properties, 1, &device_id, NULL, NULL, &error);
 
+	context = clCreateContext(properties,1, &device_id, NULL, NULL, &error);
+	if (error != CL_SUCCESS) {
+		printf("cannot create context\n");
+		return 1;
+	}
 	/*command queue-> for sendig comman to the device 
 	 *
 	 *The command queue takes memory writes and reads, and kernel execution
@@ -99,89 +132,105 @@ int main (int argc , char ** argv )
 	 * i.e. execution is controlled by command queue
 	 * */
 
-	cl_command_queue command_queue;
 	command_queue = clCreateCommandQueue(context, device_id, 0, &error);
 	if (error != CL_SUCCESS) {
-		printf("cannot create context\n");
+		printf("cannot create command queue\n");
 		return 1;
 	}
 
 
-	/*open cl program command stored in a string */
-
-	cl_program program;
-	program = clCreateProgramWithSource(context,1,
-			(const char **) &opencl_program_src, NULL, &error);
-
-	clBuildProgram(program, 0, NULL, NULL, NULL, NULL)
-
-		cl_kernel kernel;
-	kernel = clCreateKernel(program, "dot_prod_mul", &error);
-
-
-
-	
-
-		/*memory reserved as buffers*/
-		const size_t ix_m = 10e7;
-		cl_mem input_buffer_a, input_buffer_b, output_buffer_c;
-		input_buffer_a  = clCreateBuffer(context, CL_MEM_READ_ONLY,
-				                    sizeof(float) * ix_m, NULL, &error);
-		input_buffer_b  = clCreateBuffer(context, CL_MEM_READ_ONLY,
-				                    sizeof(float) * ix_m, NULL, &error);
-		output_buffer_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-					sizeof(float) * ix_m, NULL, &error);
-
-
-		/*three last arguments are used for synchronization ( events ) */
-		/*enqueuing writing buffers*/
-		clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,
-				  0, ix_m*sizeof(float), a, 0, NULL, NULL);
-		clEnqueueWriteBuffer(command_queue, input_buffer_b, CL_TRUE,
-				  0, ix_m*sizeof(float), b, 0, NULL, NULL);
-
-
-/*enqueuing kernel */
-		clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_a);
-		clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_b);
-		clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer_c);
-
-clEnqueueNDRangeKernel(command_queue, kernel, dimension,
-		  global_offsets, global_sizes, local_sizes, 0, NULL, NULL);
-
-
-		clReleaseMemObject(input_buffer_a);
-		clReleaseMemObject(input_buffer_b);
-		clReleaseMemObject(output_buffer_c);
-		//TODO use opencl data types
-
-
-	/*error checking -> build log of the kernel*/
-
-	error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-	if (error != CL_SUCCESS) {
-		printf("cannot build program. log:\n");
-
-		size_t log_size = 0;
-		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-				0, NULL, &log_size);
-
-		char * log = calloc(log_size, sizeof(char));
-		if (log == NULL) {
-			printf("could not allocate memory\n");
-			return 1;
-		}
-
-		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
-				log_size, log, NULL);
-		printf( "%s\n", log );
-
-		free(log);
-
+//		/*memory reserved as buffers*/
+//		const size_t ix_m = 10e7;
+//		cl_mem input_buffer_a, input_buffer_b, output_buffer_c;
+//		input_buffer  = clCreateBuffer(context, CL_MEM_READ_ONLY,
+//				sizeof(float) * ix_m, NULL, &error);
+//		output_buffer_avg  = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+//				sizeof(float) * ix_m, NULL, &error);
+//		output_buffer_matrix = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+//				sizeof(float) * ix_m, NULL, &error);
+//
+//
+//		/*three last arguments are used for synchronization ( events ) */
+//		/*enqueuing writing buffers*/
+//		clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,
+//				0, ix_m*sizeof(float), a, 0, NULL, NULL);
+//		clEnqueueWriteBuffer(command_queue, input_buffer_b, CL_TRUE,
+//				0, ix_m*sizeof(float), b, 0, NULL, NULL);
+//
+//
+//
+//	/*open cl program command stored in a string */
+//
+////	program = clCreateProgramWithSource(context,1,
+//			(const char **) &opencl_program_src, NULL, &error);
+//
+//	/*error checking -> build log of the kernel*/
+//
+//	error = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+//	if (error != CL_SUCCESS) {
+//		printf("cannot build program. log:\n");
+//
+//		size_t log_size = 0;
+//		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
+//				0, NULL, &log_size);
+//
+//		char * log = calloc(log_size, sizeof(char));
+//		if (log == NULL) {
+//			printf("could not allocate memory\n");
+//			return 1;
+//		}
+//
+//		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
+//				log_size, log, NULL);
+//		printf( "%s\n", log );
+//
+//		free(log);
+//
+//		kernel = clCreateKernel(program, "dot_prod_mul", &error);
+//
+//
+//
+//
+//		/*enqueuing kernel */
+//		clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_a);
+//		clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_b);
+//		clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer_c);
+//
+//		clEnqueueNDRangeKernel(command_queue, kernel, dimension,
+//				global_offsets, global_sizes, local_sizes, 0, NULL, NULL);
+//
+//		
+//
+//	for(size_t step=0;step<required_iterations ;step++) {
+//	
+//	
+//	
+//	}
+//
+//		/*print average temperature*/
+//		avg=avg/(h*w);
+//		fprintf(stdout,"Average temperature: %f\n",avg);
+//
+//		/*print absolute difference between entries of matrix and average tmp*/
+//		for(int i=0;i<h;i++) {
+//		for(int j=0;j<w;j++) {
+//		fprintf(stdout,"%f ",cabs(matrix[i][j]-avg));
+//		}
+//		fprintf(stdout,"\n");
+//		
+//		
+//		}
+//		clReleaseMemObject(input_buffer_a);
+//		clReleaseMemObject(input_buffer_b);
+//		clReleaseMemObject(output_buffer_c);
+//		//TODO use opencl data types
+//
+//
+//
 		/*release memory*/
-	clReleaseProgram(program);
-	clReleaseKernel(kernel);
-	clReleaseCommandQueue(command_queue);
+		clReleaseProgram(program);
+		clReleaseKernel(kernel);
+		clReleaseCommandQueue(command_queue);
 		clReleaseContext(context);
 
 		for(int i=0;i<h;i++) {
--- assignment_04/heat_diffusion.cl
+++ assignment_04/heat_diffusion.cl
@@ -1,14 +1,14 @@
-
-
-/*kernel is afunction or a task to do Saved as a separate file (typically ending in .cl) and loaded on runtime */
-
-__kernel void
-dot_prod_mul(
+__kernel void dot_prod_mul(
 	     __global const float * a,
 	     __global const float * b,
 	     __global float * c
 	     )
 {
+
+
+/*kernel is afunction or a task to do Saved as a separate file (typically ending in .cl) and loaded on runtime */
+
+
 int ix = get_global_id(0);
 c[ix] = a[ix] * b[ix];
 }
